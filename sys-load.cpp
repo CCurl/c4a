6e4db30ac5e4ac6dd4025facc94bb6c5ba7fb6a6
Chris Curl <curlc@yahoo.com>
2025-04-23 00:52:20 -0400

    more

--- sys-load.cpp
+++ sys-load.cpp
@@ -6,8 +6,8 @@ void sys_load() {
 }
 #else
 void sys_load() {
-    outer(": 0sp  0 (dsp)  ! ;");
-    outer(": 0rsp 0 (rsp)  ! ;");
+    outer(": 0sp  0 (dsp) ! ;");
+    outer(": 0rsp 0 (rsp) ! ;");
     outer(": create addword vhere lit, ;");
     outer(": does> (jmp) , r> , ;");
     outer(": hex     $10 base ! ;   : binary  %10 base ! ;");
@@ -17,8 +17,8 @@ void sys_load() {
     outer(": +! tuck  @ + swap ! ;");
     outer(": execute ( a-- ) >r ;");
     outer(": !t- t@- c! ;");
-    outer("40 var #buf");
-    outer(": <# ( n1--n2 ) #buf 39 + >t 0 !t- dup 0 < >a abs ;");
+    outer("#40 var #buf");
+    outer(": <# ( n1--n2 ) #buf #39 + >t 0 !t- dup 0 < >a abs ;");
     outer(": #. ( -- )     '.' !t- ;");
     outer(": #n ( n-- )    dup 9 > if 7 + then '0' + !t- ;");
     outer(": #  ( n1--n2 ) base @ /mod swap #n ;");
@@ -36,7 +36,6 @@ void sys_load() {
     outer(": marker here t0 ! last t1 ! vhere t2 ! ;");
     outer(": forget t0 @ (here) ! t1 @ (last) ! t2 @ (vhere) ! ;");
     outer(": fgl last de-sz + (last) ! last w@  (here) ! ;");
-
     outer("marker");
 }
 #endif // _SYS_LOAD_
